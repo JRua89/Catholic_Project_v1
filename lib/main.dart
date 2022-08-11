9d3f0152a16516a4c87fae47cf857d447c2f5d2f
John Rua <JRua89@gmail.com>
2022-08-11 19:00:35 -0400

    added text and images for app

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Oraciones Catholicas en Español',
         theme: ThemeData(
-          primarySwatch: Colors.lightBlue,
-          accentColor: Colors.lightBlue[600],
+          primarySwatch: Colors.blue,
         ),
         home: const HomePage(title: 'Home page'),
 
@@ -101,20 +100,22 @@ class _HomePageState extends State<HomePage>
             Container(
               alignment: Alignment.center,
               child: Image.asset(
-                'Assets/images/6305266170_c81dccb543_m.jpg',
-                height: 650,
+                'Assets/images/nutsisnitin-1574320526796-cathopic.jpg',
+
                 width: double.infinity,
-                fit: BoxFit.cover,
+                fit: BoxFit.fill,
               ),
             ),
             Container(
                 alignment: Alignment.center,
                 child: Text(
-                  'Para leer las oraciones oprima el menu',
-                  style: TextStyle(color: Colors.black,
-                      background: Paint()..color = Colors.lightBlue,
+                  ''
+                      ''
+                      'Para leer las oraciones oprima el menu',
+                  style: TextStyle(color: Colors.white,
+                      background: Paint()..color = Colors.lightBlue.shade900,
                       fontWeight: FontWeight.bold,
-                      fontSize: 25.0),
+                      fontSize: 18.0),
                 )),
           ],
         ),
@@ -130,7 +131,7 @@ class _HomePageState extends State<HomePage>
             ),
             child: FloatingActionButton(
               backgroundColor: Colors.blue,
-              onPressed: _opennewpage,
+              onPressed: _pageviewthird,
               child: const Text('3'),
             ),
           ),
@@ -142,7 +143,7 @@ class _HomePageState extends State<HomePage>
             ),
             child: FloatingActionButton(
               backgroundColor: Colors.red,
-              onPressed: () {/* Do something */},
+              onPressed: _pageviewsecond,
               child: const Text('2'),
             ),
           ),
@@ -154,7 +155,7 @@ class _HomePageState extends State<HomePage>
             ),
             child: FloatingActionButton(
               backgroundColor: Colors.amber,
-              onPressed: () {/* Do something */},
+              onPressed: _opennewpage,
               child: const Text('1'),
             ),
           ),
@@ -177,7 +178,7 @@ class _HomePageState extends State<HomePage>
         builder: (BuildContext context) {
           return new Scaffold(
             appBar: new AppBar(
-              title: new Text('Pagina 3'),
+              title: new Text('Pagina 1'),
             ),
             body: SingleChildScrollView(
               child: new Column(
@@ -210,7 +211,7 @@ no nos dejes caer en la tentación,
 y líbranos del mal.
                        
 Amén.
-                ''',maxLines: 20, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black,),
+                ''',maxLines: 20, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black,),
                   ),
 
                   new Text('''
@@ -236,7 +237,7 @@ Amén.
   ahora y en la ahora de nuestra muerte
                           
   Amén.
-                ''',maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black) ),
+                ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black) ),
 
                   new Text('''
                   
@@ -253,14 +254,14 @@ Amén.
                   ////Image.network('https://i.swncdn.com/media/400w/cms/CCOM/60643-catholic-priest-communion-thinkstockphotos-88.1200w.tn.jpg'),
                   new Text('''
                   
-Gloria al Padre,
-y al Hijo,
-y al Espíritu Santo.
-Como era en el principio,
-ahora y siempre, por los siglos de los siglos.
+  Gloria al Padre,
+  y al Hijo,
+  y al Espíritu Santo.
+  Como era en el principio,
+  ahora y siempre, por los siglos de los siglos.
 
-Amén
-                ''',maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black) ),
+  Amén
+                ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black) ),
 
                   Container(
                     width: double.infinity,
@@ -272,7 +273,7 @@ Amén
                       ),
                     ),
                     child: Center(
-                        child: Text("Regresa al la primera pagina para leer las otras oraciones", style: TextStyle(fontSize: 20)),
+                        child: Text(" Regresa al la primera pagina para leer las otras oraciones", style: TextStyle(fontSize: 18)),
 
                     ),
 
@@ -287,30 +288,137 @@ Amén
     );
   }
 
-  void _mainpageview() {
+  void _pageviewsecond() {
     Navigator.of(context).push(
       new MaterialPageRoute(
         builder: (BuildContext context) {
           return new Scaffold(
             appBar: new AppBar(
-              title: new Text('dsf'),
+              title: new Text('Pagina 2'),
             ),
             body: SingleChildScrollView(
               child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.network('https://i.swncdn.com/media/400w/cms/CCOM/60643-catholic-priest-communion-thinkstockphotos-88.1200w.tn.jpg'),
+                  new Text('''
+                  
+              
+   Credo
+              
+              ''',
+                      maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+
+                  Image.asset('Assets/images/cathopic_1521568181227473.jpg'),
                   new Text(
-                    'Tempor nec feugiat nisl pretium fusce id. At risus viverra adipiscing at in tellus integer feugiat. Id diam maecenas ultricies mi eget. Donec enim diam vulputate ut pharetra sit amet aliquam. Donec adipiscing tristique risus nec feugiat. Condimentum lacinia quis vel eros donec ac odio. Dui ut ornare lectus sit. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Lacus viverra vitae congue eu consequat. Pretium lectus quam id leo in vitae turpis massa sed. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Pellentesque habitant morbi tristique senectus et netus et. Quam nulla porttitor massa id. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget.',
-                  ),
-                  Image.network('https://i.swncdn.com/media/400w/cms/CCOM/60643-catholic-priest-communion-thinkstockphotos-88.1200w.tn.jpg'),
+                    '''
+                    
+  Creo en un solo Señor, Jesucristo,
+  Hijo único de Dios, nacido del Padre antes de todos los siglos:
+  Dios de Dios, Luz de Luz, Dios verdadero de Dios verdadero,
+  engendrado, no creado, de la misma naturaleza del Padre,
+  por quien todo fue hecho;
+  que por nosotros lo hombres,
+  y por nuestra salvación bajó del cielo,
+  y por obra del Espíritu Santo se encarnó de María, la Virgen,
+  y se hizo hombre;
+  y por nuestra causa fue crucificado
+  en tiempos de Poncio Pilato;
+  padeció y fue sepultado,
+  y resucitó al tercer día, según las Escrituras,
+  y subió al cielo,
+  y está sentado a la derecha del Padre;
+  y de nuevo vendrá con gloria
+  para juzgar a vivos y muertos,
+  y su reino no tendrá fin.
+
+  Creo en el Espíritu Santo,
+  Señor y dador de vida,
+  que procede del Padre y del Hijo,
+  que con el Padre y el Hijo
+  recibe una misma adoración y gloria,
+  y que habló por los profetas.
+
+  Creo en la Iglesia, que es una, santa, católica y apostólica.
+  Confieso que hay un solo bautismo
+  para el perdón de los pecados.
+  Espero la resurrección de los muertos
+  y la vida del mundo futuro.
+
+  Amén.
+  
+  
+                    ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  new Text('''
+                  
+              
+   Salve
+              
+              ''',
+                      maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  Image.asset('Assets/images/mario_sdb-1564458703747-cathopic.jpg'),
                   new Text(
-                    'Tempor nec feugiat nisl pretium fusce id. At risus viverra adipiscing at in tellus integer feugiat. Id diam maecenas ultricies mi eget. Donec enim diam vulputate ut pharetra sit amet aliquam. Donec adipiscing tristique risus nec feugiat. Condimentum lacinia quis vel eros donec ac odio. Dui ut ornare lectus sit. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Lacus viverra vitae congue eu consequat. Pretium lectus quam id leo in vitae turpis massa sed. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Pellentesque habitant morbi tristique senectus et netus et. Quam nulla porttitor massa id. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget.',
-                  ),
-                  Image.network('https://i.swncdn.com/media/400w/cms/CCOM/60643-catholic-priest-communion-thinkstockphotos-88.1200w.tn.jpg'),
+                    '''            
+                         
+  Dios te salve,
+  Reina y Madre de misericordia,
+  vida, dulzura y esperanza nuestra.
+  Dios te salve.
+  A Tí clamamos los desterrados hijos de Eva,
+  a Tí suspiramos, gimiendo y llorando en este valle de lágrimas.
+  Ea, pues, Señora Abogada Nuestra,
+  vuelve a nosotros tus ojos misericordiosos,
+  y después de este destierro, muéstranos a Jesús,
+  fruto bendito de tu vientre.
+  Oh, clemente, oh piadosa,
+  oh dulce Virgen María.
+  Ruega por nosotros, Santa Madre de Dios,
+  para que seamos dignos de alcanzar las promesas
+  de Nuestro Señor Jesucristo.
+
+  Amén
+  
+                    ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  new Text('''
+                  
+              
+   Angel de la Guarda
+              
+              ''',
+                      maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  Image.asset('Assets/images/tachodimas-1559494568976-cathopic.jpg'),
                   new Text(
-                    'Tempor nec feugiat nisl pretium fusce id. At risus viverra adipiscing at in tellus integer feugiat. Id diam maecenas ultricies mi eget. Donec enim diam vulputate ut pharetra sit amet aliquam. Donec adipiscing tristique risus nec feugiat. Condimentum lacinia quis vel eros donec ac odio. Dui ut ornare lectus sit. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Lacus viverra vitae congue eu consequat. Pretium lectus quam id leo in vitae turpis massa sed. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Pellentesque habitant morbi tristique senectus et netus et. Quam nulla porttitor massa id. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget.',
+                    '''
+                    
+  Ángel de mi guarda dulce compañía
+  no me desampares ni de noche ni de día.
+  No me dejes sólo que sin ti me perdería.
+  Hasta que me entregues en los brazos
+  de Jesus y de Maria
+
+  Amén
+                    ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.lightBlue,
+                        width: 5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(" Regresa al la primera pagina para leer las otras oraciones", style: TextStyle(fontSize: 18)),
+
+                    ),
+
                   ),
+
                 ],
               ),
             ),
@@ -319,5 +427,103 @@ Amén
       ),
     );
   }
+
+
+
+  void _pageviewthird() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (BuildContext context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Pagina 2'),
+            ),
+            body: SingleChildScrollView(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  new Text('''
+                  
+              
+   Yo confieso
+
+              
+              ''',
+                      maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+
+                  Image.asset('Assets/images/vytas_sdb-1569046036181-cathopic.jpg'),
+                  new Text(
+                      '''
+                    
+  Yo confieso ante Dios Todopoderoso,
+  y ante ustedes hermanos
+  que he pecado mucho de pensamiento,
+  palabra, obra y omisión.
+  Por mi culpa, por mi culpa, por mi gran culpa.
+  Por eso ruego a Santa María siempre Virgen,
+  a los ángeles, a los santos y a ustedes hermanos,
+  que intercedan por mí ante Dios, Nuestro Señor.
+
+  Amén.
+  
+  
+                    ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  new Text('''
+                  
+              
+   Acto de Contrición
+              
+              ''',
+                      maxLines: 50, style: TextStyle(fontSize: 20.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+                  Image.asset('Assets/images/acto-de-contrición.jpg'),
+                  new Text(
+                      '''            
+                         
+  Señor mío, Jesucristo,
+  Dios y hombre verdadero,
+  me pesa de todo corazón haberte ofendido;
+  propongo firmemente nunca más pecar,
+  apartarme de todas las ocasiones de pecado,
+  confesarme y cumplir la penitencia.
+  Confío que por tu infinita misericordia
+  me has de conceder el perdón de mis culpas
+  y me has de llevar a la vida eterna.
+  Te ofrezco, Señor, mi vida,
+  obras y trabajos en satisfacción de todos mis pecados.
+
+
+  Amén.
+  
+                    ''',maxLines: 50, style: TextStyle(fontSize: 18.0 ,fontWeight:FontWeight.bold,color: Colors.black)),
+
+
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.lightBlue,
+                        width: 5,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(" Regresa al la primera pagina para leer las otras oraciones", style: TextStyle(fontSize: 18)),
+
+                    ),
+
+                  ),
+
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
 
 }
